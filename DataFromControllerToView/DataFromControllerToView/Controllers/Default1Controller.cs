3efa7540af5ed1d7b0ccb8151c580feef9ba816d
Sayed Mahmood Ghaemmaghami <63294201+mahmood-ghaem@users.noreply.github.com>
2020-06-15 15:59:58 +0200

    add DataFromControllerToView
    session
    cookie
    viewbag
    viewdata
    tempdata

---
+++ DataFromControllerToView/DataFromControllerToView/Controllers/Default1Controller.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataFromControllerToView.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewBagDemo()
+        {
+            ViewBag.myData1 = "Hello World! from ViewBag";
+            return View();
+        }
+
+        public IActionResult ViewDataDemo()
+        {
+            ViewData["myData2"] = "Hello World! from ViewData";
+            return View();
+        }
+
+
+        public IActionResult TempDataDemoSend()
+        {
+            TempData["myData3"] = "Hello World! from TempData";
+            return RedirectToAction("TempDataDemoGet", "Default2");
+        }
+
+        [HttpGet]
+        public IActionResult SetSession()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SetSession(string name)
+        {
+            HttpContext.Session.SetString("sessionuser", name);
+            return RedirectToAction("GetSession", "Default2");
+        }
+
+        //usefull reference: https://asp.mvc-tutorial.com/httpcontext/cookies/ 
+        public IActionResult ReadWriteCookie()
+        {
+            if (!HttpContext.Request.Cookies.ContainsKey("first_request"))
+            {
+                CookieOptions cookieOptions = new CookieOptions();
+                cookieOptions.Expires = new DateTimeOffset(DateTime.Now.AddDays(7));
+                HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString(), cookieOptions);
+                return Content("Welcome, new visitor!");
+            }
+            else
+            {
+                DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["first_request"]);
+                return Content("Welcome back, user! You first visited us on: " + firstRequest.ToString());
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ DataFromControllerToView/DataFromControllerToView/Controllers/Default2Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataFromControllerToView.Controllers
+{
+    public class Default2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TempDataDemoGet()
+        {
+            return View();
+        }
+        public IActionResult GetSession()
+        {
+            return View();
+        }
+    }
+}
